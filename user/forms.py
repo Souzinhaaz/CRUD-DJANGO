d0cd6141e19523f175699a9eaf86c4fbac92ee3e
Souzinhaaz <gustavodesouzaoliveira1432@gmail.com>
2024-07-11 15:42:20 -0300

    feature: Added message and improved forms

--- user/forms.py
+++ user/forms.py
@@ -1,7 +1,32 @@
 from django import forms
+from django.forms import TextInput, EmailInput, PasswordInput, DateInput
 from .models import User
 
 class UserModelForm(forms.ModelForm):
     class Meta:
         model = User
         fields = "__all__"
+        widgets = {
+            "name": TextInput(attrs={
+                "class": "form-control mt-2 w-100",
+                "placeholder": "Informe o seu nome"
+            }),
+            "email": EmailInput(attrs={
+                "class": "form-control mt-2 w-100",
+                "placeholder": "Informe o seu email"
+            }),
+            "password": TextInput(attrs={
+                "type": "password",
+                "class": "form-control mt-2 w-100",
+                "placeholder": "Informa a sua senha",
+            }),
+            "created_at": DateInput(attrs={
+                "style": "display: none;",
+            })
+        }
+        
+        def clean_password(self):
+            password = self.cleaned_data.get("password")
+            if not password:
+                self.add_error("value", "O campo da senha deve ser preenchido.")
+            return password
--- user/views.py
+++ user/views.py
@@ -1,24 +1,38 @@
 from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .models import User
 from .forms import UserModelForm
 
-class UsersCreateView(CreateView):
+class UsersCreateView(SuccessMessageMixin, CreateView):
     model = User
     form_class = UserModelForm
-    template_name = 'user/user_form.html'
+    template_name = 'new_user.html'
     success_url = '/users/'
+    success_message = "%(name)s foi criado com sucesso!"
 
 class UsersListView(ListView):
     model = User
-    template_name = 'user/users.html'
+    template_name = 'users.html'
     context_object_name = 'usuarios'
+    
+    def get_queryset(self):
+        users = super().get_queryset().order_by('name')
+        search = self.request.GET.get('search')
+        if search:
+            users = users.filter(name__icontains=search)
+        return users
 
-class UsersUpdateView(UpdateView):
+class UsersUpdateView(SuccessMessageMixin, UpdateView):
     model = User
-    fields = ["name", "email", "created_at"]
+    form_class = UserModelForm
+    template_name = 'user_update.html'
     success_url = reverse_lazy('home')
+    success_message = "%(name)s foi atualizado com sucesso!"
 
-class UsersDeleteView(DeleteView):
+class UsersDeleteView(SuccessMessageMixin, DeleteView):
     model = User
+    template_name = 'user_delete.html'
+    context_object_name = 'usuario'
     success_url = reverse_lazy('home')
+    success_message = "O usuário foi deletado com sucesso!"
